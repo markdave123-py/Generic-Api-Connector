25601412b9bd879101e6c55f2335507e95d4d8c1
markdave123-py <nwaekwudavid@gmail.com>
2025-05-19 10:45:27 +0100

    docs: update README with plug-in architecture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 import pytest_asyncio
 from httpx import AsyncClient
 from simapi.main import app as fastapi_app
-from connector.client import APIClient
 from httpx._transports.asgi import ASGITransport
 from connector.registry import get_connector
 
